41e481dde45c8aa0b7f0100ca2ca406ceb9dfd99
Scott Pearson <pearson.s00@bigpond.com>
2013-10-01 18:38:11 +0800

    Upload Proj

---
+++ PictureViewer/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            // Show the Open File dialog. If the user clicks OK
+            // then load the picture the user chooses.
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // Clear the picture.
+            pictureBox1.Image = null;
+        }
+
+        private void backgroundButton_Click(object sender, EventArgs e)
+        {
+            // Show the colour dialog box. If the user clicks OK, 
+            // change the Picture Box's background to the colour the user chose.
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            // If the user selects the stretch checkbox,
+            // change the Picture Box's SizeMode to "stretch".
+            // If the user clears the checkbox then set to "normal".
+            if (checkBox1.Checked)
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            else
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+        }
+    }
+}
